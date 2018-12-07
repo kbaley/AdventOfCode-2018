d50a1826cefee5b6b8e53b2087f04412bc4240fe
Kyle Baley <kyle@baley.org>
2018-12-07 18:55:27 -0500

    Boilerplating

---
+++ day10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = File.ReadAllLines("./input.txt");
+
+            foreach (var input in inputs)
+            {
+                
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
