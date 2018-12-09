3104913e7970e9502f065cba5c1fe3019b92b865
Kyle Baley <kyle@baley.org>
2018-12-09 08:43:37 -0500

    Day 8 and more boilerplate

---
+++ day12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = File.ReadAllLines("./input.txt");
+
+            foreach (var input in inputs)
+            {
+                
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
--- day8/Program.cs
+++ day8/Program.cs
@@ -7,16 +7,67 @@ namespace day8
 {
     class Program
     {
+            static int x = 0;
+            static int totalMeta = 0;
         static void Main(string[] args)
         {
             var inputs = File.ReadAllLines("./input.txt");
+            var entries = inputs[0].TheDylanSplit().Select(int.Parse).ToList();
 
-            foreach (var input in inputs)
-            {
-                
-            }
+            var root = Moo(entries);
+
+            System.Console.WriteLine($"::: PART ONE ::: {totalMeta}");
+            System.Console.WriteLine($"::: PART TWO ::: {root.value}");
+
+
             Console.WriteLine("Done");
-            Console.ReadLine();
+        }
+
+        static Node Moo(List<int> entries) {
+
+            var node = new Node{
+                numChildren = entries[x++],
+                numMeta = entries[x++]
+            };
+            for (int i = 0; i < node.numChildren; i++)
+            {
+                node.children.Add(Moo(entries));
+            }
+            for (int i = 0; i < node.numMeta; i++)
+            {
+                var metadata = entries[x++];
+                node.metadata.Add(metadata);
+                totalMeta += metadata;
+            }
+            return node;
+        }
+
+    }
+
+    public class Node {
+        public int numChildren;
+        public int numMeta;
+        public List<Node> children = new List<Node>();
+        public List<int> metadata = new List<int>();
+
+        public int value {
+            get {
+                if (numChildren == 0) return metadata.Sum();
+                var result = 0;
+                foreach (var item in metadata)
+                {
+                    if (item <= children.Count()) {
+                        result += children[item - 1].value;
+                    }   
+                }
+                return result;
+            }
+        }
+    }
+
+    static class Extensions {
+        public static string[] TheDylanSplit(this string value) {
+            return value.Split(' ');
         }
     }
 }
