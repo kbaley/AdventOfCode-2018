a95c60d9458e0fb96ea3a248184e12b09af14f8d
Kyle Baley <kyle@baley.org>
2018-12-10 10:33:08 -0500

    Day 10

--- day10/Program.cs
+++ day10/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace day10
 {
@@ -10,12 +11,78 @@ namespace day10
         static void Main(string[] args)
         {
             var inputs = File.ReadAllLines("./input.txt");
+            var pointList = new List<(Point point, Point velocity)>();
 
             foreach (var input in inputs)
             {
-                
+                var data = input
+                    .Replace("position=<", "")
+                    .Replace("> velocity=<", "~~")
+                    .Replace(">", "")
+                    .Split("~~");
+                pointList.Add((data[0].InputToPoint(), data[1].InputToPoint()));
             }
+            var points = pointList.ToArray();
+
+            var lastBounds = long.MaxValue;
+            var bounds = Bounds(points.Select(p => p.point));
+            var secs = 0;
+            while (bounds < lastBounds)
+            {
+                lastBounds = bounds;
+                for (var i = 0; i < points.Length; i++)
+                {
+                    points[i].point.X += points[i].velocity.X;
+                    points[i].point.Y += points[i].velocity.Y;
+                }
+                bounds = Bounds(points.Select(p => p.point));
+                secs++;
+            }
+            for (var i = 0; i < points.Length; i++)
+            {
+                    points[i].point.X -= points[i].velocity.X;
+                    points[i].point.Y -= points[i].velocity.Y;
+            }
+
+            ShowGrid(points.Select(p => p.point));
+            System.Console.WriteLine($"{secs - 1} seconds");
+
             Console.WriteLine("Done");
+        }
+
+        static void ShowGrid(IEnumerable<Point> points)
+        {
+
+            var minX = points.Min(p => p.X);
+            var maxX = points.Max(p => p.X);
+            var minY = points.Min(p => p.Y);
+            var maxY = points.Max(p => p.Y);
+            for (var j = minY; j <= maxY; j++)
+            {
+                for (var i = minX; i <= maxX; i++)
+                {
+                    Console.Write(points.Any(p => p.X == i && p.Y == j) ? "#" : " ");
+                }
+                System.Console.WriteLine();
+            }
+            System.Console.WriteLine();
+        }
+
+        static long Bounds(IEnumerable<Point> points)
+        {
+            var minX = points.Min(p => p.X);
+            var maxX = points.Max(p => p.X);
+            var minY = points.Min(p => p.Y);
+            var maxY = points.Max(p => p.Y);
+            return (long)(maxX - minX) * (long)(maxY - minY);
+        }
+    }
+
+    public static class Extensions
+    {
+        public static Point InputToPoint(this string input)
+        {
+            return new Point(int.Parse(input.Split(',')[0]), int.Parse(input.Split(',')[1]));
         }
     }
 }
