fb50e1d6e0878e01d0f32eeaf731d0d8989925d6
Kyle Baley <kyle@baley.org>
2018-12-11 20:22:12 -0500

    Day 11

--- day11/Program.cs
+++ day11/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace day11
 {
@@ -9,13 +10,59 @@ namespace day11
     {
         static void Main(string[] args)
         {
-            var inputs = File.ReadAllLines("./input.txt");
+            const int SERIAL_NUMBER  = 8199;
+            (var width, var height) = (300, 300);
+            var grid = new long[width, height];
 
-            foreach (var input in inputs)
-            {
-                
+            for(var x = 0; x < width; x++) {
+                for (var y = 0; y < height; y++) {
+                    grid[x,y] = PowerLevel(x, y, SERIAL_NUMBER);
+                }
             }
+
+            Point maxPoint = default(Point);
+            long maxValue = long.MinValue;
+            int maxSize = int.MinValue;
+            for (var x = 0; x < width; x++ ) {
+                for (var y = 0; y < height; y++) {
+                    for ( var i = 0; i < GetMaxSquare(x, y); i++ ) {
+                        var totalPower = TotalPower(x, y, i, grid);
+                        if (totalPower > maxValue) {
+                            maxPoint = new Point(x, y);
+                            maxValue = totalPower;
+                            maxSize = i;
+                        }
+                    }
+                }
+            }
+            System.Console.WriteLine(maxPoint.ToString());
+            System.Console.WriteLine(maxSize);
             Console.WriteLine("Done");
+        }
+
+        private static int GetMaxSquare(int x, int y)
+        {
+            return Math.Min(300 - x + 1, 300 - y + 1);
+        }
+
+        private static long TotalPower(int x, int y, int size, long[,] grid)
+        {
+            if (x + size >= 300) return long.MinValue;
+            if (y + size >= 300) return long.MinValue;
+            var totalPower = 0L;
+            for (var i = 0; i < size; i++) {
+                for (var j = 0; j < size; j++) {
+                    totalPower += grid[x + i, y + j];
+                }
+            }
+            return totalPower;
+        }
+
+        static long PowerLevel(int x, int y, int serial) {
+            var rackId = x + 10;
+            var powerLevel = (rackId * y + serial) * rackId;
+            powerLevel = (int)Math.Abs(powerLevel/100%10) - 5;
+            return powerLevel;
         }
     }
 }
