27754ad1c5d34ca06df8117096fcfafa6490ffaf
Kyle Baley <kyle@baley.org>
2018-12-14 20:28:03 -0500

    Tweaks

--- day14/Program.cs
+++ day14/Program.cs
@@ -8,59 +8,39 @@ namespace day14
 {
     class Program
     {
-        static long i = 0;
         static void Main(string[] args)
         {
             var input = "37";
-            var numRecipes = 306281; //306281;
+            var numRecipes = 306281;
             var elf1 = 0;
             var elf2 = 1;
 
-            // PartOne(input, numRecipes, elf1, elf2);
+            PartOne(input, numRecipes, elf1, elf2);
 
-            // PART TWO
-            PartTwo(input, numRecipes.ToString(), elf1, elf2);
+            PartTwo(input, numRecipes.ToString());
 
             Console.WriteLine("Done");
         }
 
-        private static void PartTwo(string input, string numRecipes, int e1, int e2)
+        private static void PartTwo(string input, string numRecipes)
         {
             var list = new LinkedList<int>();
             var elf1 = list.AddFirst(3);
             var elf2 = list.AddLast(7);
-            while (!Contains(list, numRecipes))
+            var last10 = input;
+            while (!last10.Contains(numRecipes))
             {
                 var sum = (elf1.Value + elf2.Value).ToString();
+                last10 += sum;
+                if (last10.Length > 10) {
+                    last10 = last10.Substring(last10.Length - 10);
+                }
                 list.AddLast(int.Parse(sum[0].ToString()));
                 if (sum.Length > 1) list.AddLast(int.Parse(sum[1].ToString()));
-                var elf1Value = elf1.Value;
-                var elf2Value = elf2.Value;
-                for (int i = 0; i < 1 + elf1Value; i++)
-                {
-                    elf1 = elf1.NextInCircle();    
-                }
-                for (int i = 0; i < 1 + elf2Value; i++)
-                {
-                    elf2 = elf2.NextInCircle();    
-                }
-                if (i++ % 1000000 == 1000) System.Console.WriteLine("end: " + i + ": " + DateTime.Now);
+                elf1 = elf1.NextNInCircle(elf1.Value + 1);
+                elf2 = elf2.NextNInCircle(elf2.Value + 1);
             }
-            var newInput = string.Join("", list.Select(x => x.ToString()).ToArray());
-            System.Console.WriteLine(newInput.IndexOf(numRecipes));
-        }
-
-        private static bool Contains(LinkedList<int> list, string check)
-        {
-            if (list.Count < 12) return false;
-            var posNode = list.Last;
-            var checkStr = "";
-            for(var i = 0; i < 10; i++) {
-                checkStr += posNode.Value.ToString();
-                posNode = posNode.Previous;
-            }
-            checkStr = new string(checkStr.Select(x => x).Reverse().ToArray());
-            return checkStr.Contains(check);
+            System.Console.WriteLine(list.Count - 10 + list.Get10Back().IndexOf(numRecipes));
         }
 
         private static void PartOne(string input, int numRecipes, int elf1, int elf2)
@@ -71,7 +51,6 @@ namespace day14
                 input += sum.ToString();
                 elf1 = (elf1 + 1 + int.Parse(input[elf1].ToString())) % input.Length;
                 elf2 = (elf2 + 1 + int.Parse(input[elf2].ToString())) % input.Length;
-                // DumpIt(input, elf1, elf2);
             }
 
             var scores = input.Substring(numRecipes, 10);
@@ -96,6 +75,24 @@ namespace day14
 
         public static LinkedListNode<int> PrevInCircle(this LinkedListNode<int> node) {
             return node.Previous ?? node.List.Last;
+        }
+        
+        public static LinkedListNode<int> NextNInCircle(this LinkedListNode<int> node, int n) {
+            for (int i = 0; i < n; i++)
+            {
+                node = node.NextInCircle();     
+            }
+            return node;
+        }
+
+        public static string Get10Back(this LinkedList<int> list) {
+            var posNode = list.Last;
+            var checkStr = "";
+            for(var i = 0; i < 10; i++) {
+                checkStr += posNode.Value.ToString();
+                posNode = posNode.Previous;
+            }
+            return new string(checkStr.Select(x => x).Reverse().ToArray());
         }
     }
 
