c2b7ac9d416c31f8a5c6ed46edee5e8458f98df6
Kyle Baley <kyle@baley.org>
2018-12-20 16:12:05 -0500

    Day 20 part 1

--- day20/Program.cs
+++ day20/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace day20
 {
@@ -10,12 +11,63 @@ namespace day20
         static void Main(string[] args)
         {
             var inputs = File.ReadAllLines("./input.txt");
+            var input = inputs[0];
 
-            foreach (var input in inputs)
-            {
-                
-            }
+            var length = GetLength(input).Max();
+            System.Console.WriteLine("PART ONE:::" + length);
+
             Console.WriteLine("Done");
+        }
+
+        private static string GetSection(string input, int pos) {
+            if (input[pos] != '(') return input;
+            var start = 1;
+            var posStart = pos;
+            pos++;
+            while (start > 0) {
+                if (input[pos] == ')') start--;
+                if (input[pos] == '(') start++;
+                pos++;
+            }
+
+            return input.Substring(posStart, pos - posStart);
+        }
+
+        private static IEnumerable<int> GetLength(string input) {
+            var lengths = new List<int>();
+
+            var pos = 0;
+            var s = input.Substring(pos + 1, input.Length - pos - 2);
+            var length = 0;
+            while (pos < s.Length) {
+                switch (s[pos])
+                {
+                    case 'N':
+                    case 'E':
+                    case 'W':
+                    case 'S':
+                        length++;
+                        break;
+                    case '|':
+                        lengths.Add(length);
+                        length = 0;
+                        break;
+                    case '(':
+                        var section = GetSection(s, pos);
+                        var childLengths = GetLength(section);
+                        length += childLengths.Min() == 0 ? 0 : childLengths.Max();
+                        pos += section.Length - 1;
+                        break;
+                    case ')':
+                        lengths.Add(length);
+                        return lengths;
+                    default:
+                        break;
+                }
+                pos++;
+            }
+            lengths.Add(length);
+            return lengths;
         }
     }
 }
